165cce9ff305433c370fb0089d9765f5aabe48c5
Seunghwan-Seo1 <oosssok1@gmail.com>
2024-10-29 19:55:35 +0900

    starter

---
+++ backend/src/test/java/com/codebase/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebase.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
