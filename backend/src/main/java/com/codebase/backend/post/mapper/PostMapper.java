b2c5bc489b9355c81960a3279094dc62aa73c5db
JEONNHYUNSIK <wjs478wjs@naver.com>
2024-12-10 17:31:19 +0900

    1210a
    1210a

--- backend/src/main/java/com/codebase/backend/post/mapper/PostMapper.java
+++ backend/src/main/java/com/codebase/backend/post/mapper/PostMapper.java
@@ -33,17 +33,10 @@ public interface PostMapper {
     // 게시글 삭제 (성공 여부 반환)
     boolean deletePost(@Param("id") int  id);
     
-    // 좋아요 수 증가
-    void increaseLikeCount(@Param("id") int  id);
-    
-    // 좋아요 수 감소
-    void decreaseLikeCount(@Param("id") int  id);
-    
-    // 싫어요 수 증가
-    void increaseDislikeCount(@Param("id") int  id);
+	// 게시글 좋아요 싫어요
+	void updateLikes(int id);
+	void updateDislikes(int id);
 
-    // 싫어요 수 감소
-    void decreaseDislikeCount(@Param("id") int  id);
 
     // 인기 게시글 조회
     List<PopularResponse> findPopularPosts();
