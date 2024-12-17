196f5093d6bd290459e493a2e6921847d0d3b1d6
한정현 <hanjunghyun95@gmail.com>
2024-12-17 15:56:36 +0900

    fit: member name connect

--- backend/src/main/java/com/codebase/backend/review/dto/Review.java
+++ backend/src/main/java/com/codebase/backend/review/dto/Review.java
@@ -2,13 +2,11 @@ package com.codebase.backend.review.dto;
 
 import java.time.LocalDateTime;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
-@Getter
-@Setter
-@Data
+//@Getter
+//@Setter
+//@Data
 public class Review {
     private int id;
     private String title;
@@ -18,18 +16,57 @@ public class Review {
     private int views;
     private int likes;
     private int dislikes;
+    
+    // Getters and Setters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+	public int getViews() {
+		return views;
+	}
+	public void setViews(int views) {
+		this.views = views;
+	}
+	public int getLikes() {
+		return likes;
+	}
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	public int getDislikes() {
+		return dislikes;
+	}
+	public void setDislikes(int dislikes) {
+		this.dislikes = dislikes;
+	}
 
-	/*
-	 * public Integer getId(){ return id; }
-	 * 
-	 * public void setId(Integer id){ this.id = id; }
-	 * 
-	 * public String getTitle(){ return title; }
-	 * 
-	 * public void setTitle(String title){ this.title = title; }
-	 * 
-	 * public String getContent(){ return content; }
-	 * 
-	 * public void setContent(String content){ this.content = content; }
-	 */
+
 }
