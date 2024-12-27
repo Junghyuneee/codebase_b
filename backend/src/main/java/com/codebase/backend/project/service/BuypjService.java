50d50d3f87a79a7b8aeb5ae7c4c46e75f09e0888
pinusdensiflora <dawon10050@daum.net>
2024-12-27 10:36:13 +0900

    fix select query on bpj-mapper

---
+++ backend/src/main/java/com/codebase/backend/project/service/BuypjService.java
@@ -0,0 +1,17 @@
+package com.codebase.backend.project.service;
+
+import org.springframework.stereotype.Service;
+
+import com.codebase.backend.project.mapper.BuypjMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BuypjService {
+	
+	private final BuypjMapper buypjMapper;
+	
+	
+
+}
