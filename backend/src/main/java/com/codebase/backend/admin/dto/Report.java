65dd7bb232b1664d68f0aa6bd7fd0f44c8e0f8d6
eunji <kuji5256@gmail.com>
2024-12-29 17:56:41 +0900

    fix: 신고 카테고리 제목 join
    - 각 카테고리별 테이블을 join해서 제목 가져오기
    - 신고 테이블에 저장 시 제목 수정하면 적용이 안되는 문제 해결책

--- backend/src/main/java/com/codebase/backend/admin/dto/Report.java
+++ backend/src/main/java/com/codebase/backend/admin/dto/Report.java
@@ -17,17 +17,12 @@ public class Report {
 
     private int category; // 프로젝트 0, 게시글 1, 게시글 댓글 2, 리뷰 3
     private int categoryId; // 각각 프로젝트, 게시글 id
+    private String categoryTitle;
 
     private int count; // 신고 수
 
     private boolean completed; // 신고 처리
 
-    public Report(int reportId, int category, int categoryId, int count, boolean completed) {
-        this.reportId = reportId;
-        this.category = category;
-        this.categoryId = categoryId;
-        this.count = count;
-        this.completed = completed;
-    }
+    public Report() {} // MyBatis는 기본 생성자가 없으면 매핑할 때 객체 생성에 어려움을 겪을 수 있습니다.
 
 }
